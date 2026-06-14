9c9c83b88de9ee597ad406d90600f1635e295739
Prasannab4362 <prasannamb4362@gmail.com>
2026-06-15 01:21:35 +0530

    fix: resolve bufferAttribute TypeScript type error on build

--- src/components/CinematicLayer/CinematicLayer.tsx
+++ src/components/CinematicLayer/CinematicLayer.tsx
@@ -90,15 +90,11 @@ const ParticleSwarm = () => {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={particleCount}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
         <bufferAttribute
           attach="attributes-color"
-          count={particleCount}
-          array={colors}
-          itemSize={3}
+          args={[colors, 3]}
         />
       </bufferGeometry>
       <pointsMaterial
